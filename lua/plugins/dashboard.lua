536d22207686bd90b5e28ea02fd041942a0c63db
Arturo <git.lunchroom670@slmails.com>
2025-07-30 00:03:28 -0400

    add dashboard setup. handle stylix colorschemes in nix config

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,79 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  requires = {'nvim-tree/nvim-web-devicons'},
+  config = function()
+    local lazy_stats = require("lazy").stats()
+
+    require('dashboard').setup {
+      theme = 'doom', -- 'doom' or 'hyper' are the most used themes
+      config = {
+
+        header = {
+          "", "", "", "", "", "", "", "", "", "", -- vertical padding
+
+          "",
+          " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
+          " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
+          " ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
+          " ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+          " ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
+          " ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
+          "",
+          "Welcome to Neovim 🚀",
+          "",
+          "",
+        },
+        
+        center = {
+          {
+            icon = "📂  ",
+            desc = "Recently opened files  ",
+            key = "r",
+            action = "Telescope oldfiles",
+          },
+          {
+            icon = "🔍  ",
+            desc = "Find File              ",
+            key = "f",
+            action = "Telescope find_files",
+          },
+          {
+            icon = "🗂️  ",
+            desc = "File Browser",
+            key = "b",
+            action = "Neotree float toggle",
+          },
+          {
+            icon = "📖  ", -- thanks Chris
+            desc = "Find Word              ",
+            key = "w",
+            action = "Telescope live_grep",
+          },
+          {
+            icon = "❄️  ",
+            desc = "Open Nix Config        ",
+            key = "d",
+            action = "Telescope find_files cwd=~/.config/nix-config",
+          },
+          {
+            icon = "⬆️  ",
+            desc = "Update Plugins         ",
+            key = "u",
+            action = "Lazy update",
+          },
+        },
+
+        footer = function()
+          local count = lazy_stats.count or 0
+          return {
+            "",
+            string.format("📦  Neovim loaded %d plugins", count),
+            "",
+          }
+        end,
+      },
+    }
+  end,      
+  }
+
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,3 +5,4 @@ vim.opt.softtabstop = 2       -- Number of spaces for editing tabs (e.g., when p
 vim.opt.shiftwidth = 2        -- Number of spaces used for auto-indent (>> << etc.)
 vim.opt.number = true         -- Shows absolute line numbers for all lines 
 vim.opt.relativenumber = true -- Shows relative numbers except current line
+vim.opt.termguicolors = true
