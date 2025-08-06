8b310c63db17d16712015bd6e952ac1e00a6f043
Arturo Galan <arturo@Arturos-MacBook-Pro.local>
2025-08-05 20:02:53 -0400

    fix stylix issues

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -5,4 +5,5 @@ vim.opt.softtabstop = 2 -- Number of spaces for editing tabs (e.g., when pressin
 vim.opt.shiftwidth = 2 -- Number of spaces used for auto-indent (>> << etc.)
 vim.opt.number = true -- Shows absolute line numbers for all lines
 vim.opt.relativenumber = true -- Shows relative numbers except current line
+vim.opt.wrap = true
 vim.opt.termguicolors = true
